ef527fdb96a7e100b7db4ee0833605eb95b7846e
sosaadat <saadat@umich.edu>
2018-03-05 17:49:46 -0500

    fixed testing

--- test_rpn.py
+++ test_rpn.py
@@ -15,17 +15,19 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
-
-class TestCarrat(unittest.TestCase):
-    def test_add(self):
+#Testing carrat
+    def test_carrat1(self):
         result = rpn.calculate("1 1 ^")
         self.assertEqual(1, result)
-    def test_subtract(self):
+    def test_carrat2(self):
         result = rpn.calculate("4 0 ^")
         self.assertEqual(1, result)
-    def test_multiply(self):
+    def test_carrat3(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
-    def test_divide(self):
+    def test_carrat4(self):
         result = rpn.calculate("0 1 ^")
         self.assertEqual(0, result)
+
+
+
